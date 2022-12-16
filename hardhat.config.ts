68a0a7ecbab172a71c3f5b3ce4fe63ec90bd0830
shloyem <39220943+Shloyem@users.noreply.github.com>
2022-12-16 11:46:15 +0200

    Add Infura mainnet fork config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -7,6 +11,12 @@ const config: HardhatUserConfig = {
     ganache: {
       url: "http://127.0.0.1:8545",
       chainId: 1337
+    },
+    hardhat: {
+      forking: {
+        url: "https://mainnet.infura.io/v3/" + INFURA_API_KEY,
+        blockNumber: 16690099,
+      }
     }
   }
 };
